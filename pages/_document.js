50e3ba66c2a94ac79c1ee7715805388953c4e8da
Bertrand Marron <bertrand.marron@gmail.com>
2019-03-18 11:53:22 +0100

    Add viewport

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,8 @@ class Document extends NextDocument {
     return (
       <html>
         <Head>
+          <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+
           <style>{`
             html {
               height: 100%;
