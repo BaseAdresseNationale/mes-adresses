3704976485046ff16cdfebb212b079fdfae6f7c1
tmerlier <tmerlier@student.42.fr>
2019-05-03 14:34:43 +0200

    Add error hook

---
+++ hooks/error.js
@@ -0,0 +1,16 @@
+import {useState, useEffect} from 'react'
+import {toaster} from 'evergreen-ui'
+
+export default function useError(error) {
+  const [err, setError] = useState(error)
+
+  useEffect(() => {
+    if (err) {
+      toaster.danger('Une erreur est survenue', {
+        description: err
+      })
+    }
+  }, [err])
+
+  return [setError]
+}
