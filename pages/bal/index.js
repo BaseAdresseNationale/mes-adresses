035fb2196f36ddfdc6058873b031ed2c16b6d359
tmerlier <tmerlier@student.42.fr>
2019-05-29 15:15:44 +0200

    Add number of commune in bal

--- pages/bal/index.js
+++ pages/bal/index.js
@@ -1,7 +1,7 @@
 import React, {useState, useCallback, useContext} from 'react'
 import PropTypes from 'prop-types'
 import Router from 'next/router'
-import {Pane, Heading, Button, Table} from 'evergreen-ui'
+import {Pane, Heading, Button, Table, Text} from 'evergreen-ui'
 
 import {addCommune, removeCommune, populateCommune} from '../../lib/bal-api'
 import {getCommune} from '../../lib/geo-api'
@@ -60,10 +60,12 @@ const Index = React.memo(({baseLocale, defaultCommunes}) => {
     <>
       <Pane
         display='flex'
+        flexDirection='column'
         background='tint1'
         padding={16}
       >
         <Heading>{baseLocale.nom}</Heading>
+        <Text>{communes.length} commune{communes.length > 1 ? 's' : ''}</Text>
       </Pane>
       <Pane
         flexShrink={0}
