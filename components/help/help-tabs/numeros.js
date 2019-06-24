116f4407fc22b32072f1a317551bf8fc527f5441
tmerlier <tmerlier@student.42.fr>
2019-06-24 11:12:16 +0200

    Add comment to numero doc

--- components/help/help-tabs/numeros.js
+++ components/help/help-tabs/numeros.js
@@ -148,6 +148,34 @@ const Numeros = () => {
           </SubTuto>
         </Tuto>
 
+        <Tuto title='Ajouter une note ou un commentaire'>
+          {before}
+
+          <OrderedList margin={8}>
+            <ListItem>
+                Cliquez sur le bouton <Button background='tint1' iconBefore='more' appearance='minimal' /> se situant à droite du numéro
+            </ListItem>
+            <ListItem>
+              <Pane display='flex' alignItems='center'>
+                  Dans le menu qui vient d’apparaître, choisissez
+                <Menu.Item background='tint1' marginLeft={8} icon='edit'>
+                    Modifier
+                </Menu.Item>
+              </Pane>
+            </ListItem>
+            <ListItem>
+                Remplissez le champ de texte <Strong size={500}>Commentaire</Strong> afin de laisser une note concernant le numéro
+            </ListItem>
+            <ListItem>
+              Pour enregistrer votre commentaire, cliquez sur le bouton <Button marginX={4} appearance='primary' intent='success'>Modifier</Button>
+            </ListItem>
+          </OrderedList>
+
+          <Paragraph>
+            Vous remarquerez un <Icon icon='comment' /> sur la ligne du numéro. Le survolez vous permettra de faire apparaitre le commentaire.
+          </Paragraph>
+        </Tuto>
+
         <Problems>
           <Tuto title='Je ne trouve pas de voie lorsque j’ajoute un numéro depuis la carte'>
             <Paragraph marginTop='default'>
