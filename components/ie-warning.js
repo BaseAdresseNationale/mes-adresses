2e9b5f636d176df2d0a8238ffe6aa13c64b7eb33
gllmr <gllmr@tutanota.com>
2019-10-29 16:18:44 +0100

    typo in className

--- components/ie-warning.js
+++ components/ie-warning.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const IEWarning = () => (
-		<div className='ei-warning'>
+		<div className='ie-warning'>
 			<p>
 				Votre navigateur <b>Internet Explorer</b> n'est plus supporté par notre service. <br />
 				<b>Nous vous recommandons d'utiliser un autre navigateur</b>
 			</p>
 
 			<style jsx>{`
-				.ei-warning {
+				.ie-warning {
 					display: none;
 					padding: 10px;
 					background-color: #F00;
