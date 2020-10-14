634903ccf53808e63801a49cb38f5eb8ccd3713f
Théophile Merlière <tmerlier@users.noreply.github.com>
2020-10-14 14:31:14 +0200

    Update back-to-user-bals.js

--- pages/new/back-to-user-bals.js
+++ pages/new/back-to-user-bals.js
@@ -6,7 +6,7 @@ import {getBalAccess} from '../../lib/tokens'
 const BackToUserBALs = () => {
   const access = getBalAccess()
   return Object.keys(access).length > 0 ? (
-    <BackButton appearance='primary' intent='primary' is='a' href='/'>Voir vers mes Bases Adresses Locales</BackButton>
+    <BackButton appearance='primary' intent='primary' is='a' href='/'>Voir mes Bases Adresses Locales</BackButton>
   ) : null
 }
 
