72f97019eeeb0a2cfc1f4eb4139fe78288261b38
tmerlier <theophile.merliere@gmail.com>
2020-10-27 17:42:44 +0100

    Add loading when hasRecovered is falsy

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import dynamic from 'next/dynamic'
 import Router from 'next/router'
 import {Pane, Button, Spinner, Heading} from 'evergreen-ui'
+
+import TokenContext from '../contexts/token'
 
 import Header from '../components/header'
 import Footer from '../components/footer'
@@ -17,18 +19,29 @@ const UserBasesLocales = dynamic(() => import('../components/user-bases-locales'
 })
 
 function Index() {
+  const {hasRecovered} = useContext(TokenContext)
   return (
     <Pane display='flex' flexDirection='column' height='100%'>
       <Header />
-      <Heading padding={16} size={400} color='snow' display='flex' justifyContent='space-between' alignItems='center' backgroundColor='#0053b3' flexShrink='0'>
-        Mes Bases Adresse Locales
-        <Button iconBefore='plus' onClick={() => Router.push('/new')}>Créer une Base Adresse Locale</Button>
-      </Heading>
-      <Pane height='100%' display='flex' flexDirection='column' justifyContent='center'>
-        <UserBasesLocales />
+      <Pane height='100%' display='flex' flexDirection='column'>
+        {hasRecovered ? (
+          <>
+            <Heading padding={16} size={400} color='snow' display='flex' justifyContent='space-between' alignItems='center' backgroundColor='#0053b3' flexShrink='0'>
+            Mes Bases Adresse Locales
+              <Button iconBefore='plus' onClick={() => Router.push('/new')}>Créer une Base Adresse Locale</Button>
+            </Heading>
+            <Pane height='100%' display='flex' flexDirection='column' justifyContent='center'>
+              <UserBasesLocales />
+            </Pane>
+            <DemoBALAlert />
+            <Footer />
+          </>
+        ) : (
+          <Pane height='100%' display='flex' flexDirection='column' flex={1} alignItems='center' justifyContent='center'>
+            <Spinner />
+          </Pane>
+        )}
       </Pane>
-      <DemoBALAlert />
-      <Footer />
     </Pane>
   )
 }
