7af16a72bed33517ce24ab85258a314f16633b05
tmerlier <theophile.merliere@gmail.com>
2020-10-27 16:45:38 +0100

    Use hasRecovered flag to automatically recover BAL

--- contexts/token.js
+++ contexts/token.js
@@ -1,15 +1,20 @@
 import React, {useState, useCallback, useEffect} from 'react'
 import PropTypes from 'prop-types'
 import Router from 'next/router'
+import getConfig from 'next/config'
 
 import {getBaseLocale} from '../lib/bal-api'
-import {getBalToken, storeBalAccess} from '../lib/tokens'
+import {getBalToken, getHasRecovered, setHasRecovered, storeBalAccess} from '../lib/tokens'
+
+const {publicRuntimeConfig} = getConfig()
+const EDITEUR_URL = publicRuntimeConfig.EDITEUR_URL || 'https://editeur.adresse.data.gouv.fr'
 
 const TokenContext = React.createContext()
 
 export function TokenContextProvider({balId, token, ...props}) {
   const [state, setState] = useState({
-    token: null
+    token: null,
+    hasRecovered: null
   })
 
   const verify = useCallback(async token => {
@@ -41,6 +46,18 @@ export function TokenContextProvider({balId, token, ...props}) {
       }
     }
   }, [verify, balId, token])
+
+  useEffect(() => {
+    if (state.hasRecovered === null) {
+      const hasRecovered = getHasRecovered()
+      if (hasRecovered) {
+        setState({...state, hasRecovered: true})
+      } else {
+        setHasRecovered(true)
+        Router.push(`${EDITEUR_URL}/recovery`)
+      }
+    }
+  }, [state])
 
   const reloadEmails = useCallback(async () => {
     verify(getBalToken(balId))
