f15bac524bd4c18f331443a35191192b88e7bbd2
tmerlier <theophile.merliere@gmail.com>
2020-10-28 17:57:09 +0100

    Loads only the necessary sources

--- components/map/hooks/layers.js
+++ components/map/hooks/layers.js
@@ -3,22 +3,30 @@ import {useMemo} from 'react'
 import {getVoiesLabelLayer, getVoieTraceLayer} from '../layers/voies'
 import {getNumerosPointLayer, getNumerosLabelLayer} from '../layers/numeros'
 
-function useLayers(voie, style) {
+function useLayers(voie, sources, style) {
   return useMemo(() => {
-    const layers = [
-      getNumerosPointLayer(style),
-      getNumerosLabelLayer(),
-      getVoieTraceLayer(style)
-    ]
+    const hasNumeros = sources.find(({name}) => name === 'positions')
+    const hasVoies = sources.find(({name}) => name === 'voies')
 
-    if (!voie) {
+    const layers = hasVoies ? [
+      getVoieTraceLayer(style)
+    ] : []
+
+    if (hasNumeros) {
+      layers.push(
+        getNumerosPointLayer(style),
+        getNumerosLabelLayer()
+      )
+    }
+
+    if (!voie && hasVoies) {
       layers.push(
         getVoiesLabelLayer(style)
       )
     }
 
     return layers
-  }, [voie, style])
+  }, [voie, sources, style])
 }
 
 export default useLayers
