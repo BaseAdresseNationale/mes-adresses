30ec904b0cd4bf7ecf6fd86caeb0ac556c6df5cf
gllmr <gllmr@tutanota.com>
2020-11-16 16:15:12 +0100

    Use isLoading props for Button

--- components/sub-header/demo-warning.js
+++ components/sub-header/demo-warning.js
@@ -109,7 +109,7 @@ const DemoWarning = ({baseLocale, token}) => {
               placeholder='nom@example.com'
               onChange={onEmailChange}
             />
-            <Button appearance='primary' intent='success' type='submit' >{isLoading ? 'Chargement...' : 'Sauvegarder'}</Button>
+            <Button appearance='primary' intent='success' isLoading={isLoading} type='submit' >{'Sauvegarder'}</Button>
           </form>
         </Dialog>
         <Button height={24} marginX='.5em' onClick={() => setIsShown(true)}>Sauvegarder mes modifications</Button>
