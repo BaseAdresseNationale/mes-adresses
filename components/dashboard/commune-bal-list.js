e5a1d1fe3bc5c3b6ec2065827a89aa57cc57a1db
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2020-11-17 12:07:53 +0100

    Add CommuneBalList component

---
+++ components/dashboard/commune-bal-list.js
@@ -0,0 +1,74 @@
+import React, {useCallback, useState} from 'react'
+import PropTypes from 'prop-types'
+import Router from 'next/router'
+import {Heading, Icon, Pane} from 'evergreen-ui'
+
+import BaseLocaleCard from '../bases-locales-list/base-locale-card'
+
+const CommuneBALList = ({nomCommune, basesLocales}) => {
+  const [isOpen, setIsOpen] = useState(basesLocales.length === 1)
+
+  const handleIsOpen = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const onBalSelect = useCallback(bal => {
+    if (bal.communes.length === 1) {
+      Router.push(
+        `/bal/commune?balId=${bal._id}&codeCommune=${bal.communes[0]}`,
+        `/bal/${bal._id}/communes/${bal.communes[0]}`
+      )
+    } else {
+      Router.push(`/bal?balId=${bal._id}`, `/bal/${bal._id}`)
+    }
+  }, [])
+
+  return (
+    <Pane>
+      <Pane
+        padding={8}
+        display={isOpen ? '' : 'flex'}
+        justifyContent={isOpen ? '' : 'space-between'}
+      >
+        <Pane
+          cursor='pointer'
+          display='flex'
+          alignItems='center'
+          onClick={handleIsOpen}
+        >
+          <Icon icon={isOpen ? 'chevron-down' : 'chevron-right'} size={30} marginY='auto' />
+          <Heading size={600}>
+            {nomCommune}
+          </Heading>
+        </Pane>
+
+        {!isOpen && (
+          <Pane>
+            <Heading size={600}>
+              {`${basesLocales.length > 1 ? `${basesLocales.length} Bases Adresses Locales` : `${basesLocales.length} Base Adresse Locale`}`}
+            </Heading>
+          </Pane>
+        )}
+
+      </Pane>
+      {isOpen && (
+        <Pane>
+          {basesLocales.map(bal => (
+            <BaseLocaleCard
+              key={bal._id}
+              baseLocale={bal}
+              onSelect={() => onBalSelect(bal)}
+            />
+          ))}
+        </Pane>
+      )}
+    </Pane>
+  )
+}
+
+CommuneBALList.propTypes = {
+  nomCommune: PropTypes.string.isRequired,
+  basesLocales: PropTypes.array.isRequired
+}
+
+export default CommuneBALList
