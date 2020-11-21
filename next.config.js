152f2af4d3c67409268db503c777bc6e1ef5a557
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-11-21 18:20:30 +0100

    Remove webpack-bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-const {join} = require('path')
 const nextRuntimeDotenv = require('next-runtime-dotenv')
 
 const withConfig = nextRuntimeDotenv({
@@ -13,18 +12,7 @@ const withConfig = nextRuntimeDotenv({
 })
 
 module.exports = withConfig({
-  webpack(config, {dev, isServer}) {
-    if (!dev && !isServer) {
-      const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
-
-      config.plugins.push(new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        openAnalyzer: false,
-        reportFilename: join(__dirname, 'reports/bundles.html'),
-        defaultSizes: 'gzip'
-      }))
-    }
-
+  webpack(config) {
     config.node = {
       ...config.node,
       fs: 'empty'
