67c07225f6180ba3f21c7185156c1cd0016f867e
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-11-24 00:16:15 +0100

    Ignore moment locales except fr

--- next.config.js
+++ next.config.js
@@ -3,11 +3,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
-  webpack(config) {
+  webpack(config, {webpack}) {
     config.node = {
       ...config.node,
       fs: 'empty'
     }
+
+    config.plugins.push(new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /fr/))
 
     return config
   }
