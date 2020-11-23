bc5fd5777f4a69f823ed49aae820811e6ce5c722
tmerlier <theophile.merliere@gmail.com>
2020-11-23 11:43:16 +0100

    Change and fix back button for Safari

--- components/layout/dashboard.js
+++ components/layout/dashboard.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 import Router from 'next/router'
-import {Heading, Pane, Button} from 'evergreen-ui'
+import {Heading, Pane, Icon} from 'evergreen-ui'
 
 import Map from '../dashboard/map'
 import Header from '../header'
@@ -20,32 +20,38 @@ const propTypes = {
   children: PropTypes.node.isRequired
 }
 
-const BackButton = () => (
-  <Button
-    display='flex'
-    justifyContent='center'
-    width='100%'
-    height={42}
-    marginRight={16}
-    appearance='minimal'
-    iconBefore='arrow-left'
-    onClick={() => Router.push('/dashboard')}
-  >
-    Retour
-  </Button>
-)
+const BackButton = () => {
+  const [isHovered, setIsHovered] = useState(false)
+
+  return (
+    <Heading
+      textAlign='center'
+      cursor='pointer'
+      size={500}
+      margin={0}
+      padding='1em'
+      color='#fff'
+      backgroundColor={isHovered ? '#66788a' : '#435a6f'}
+      onClick={() => Router.push('/dashboard')}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <Icon icon='arrow-left' style={{verticalAlign: 'middle'}} marginRight={4} /> Retour France métropolitaine
+    </Heading>
+  )
+}
 
 const Mobile = ({title, backButton, mapData, children}) => {
   return (
     <Pane display='flex' backgroundColor='#fff' flexDirection='column' width='100%'>
       <Header />
 
-      <Heading size={600} margin={0} padding='1em' textAlign='center' backgroundColor='#f9f9fb'>
-        {title}
-      </Heading>
       {backButton && (
         <BackButton />
       )}
+      <Heading size={600} margin={0} padding='1em' textAlign='center' backgroundColor='#f9f9fb'>
+        {title}
+      </Heading>
 
       {children}
 
@@ -64,12 +70,12 @@ const Desktop = ({title, mapData, backButton, children}) => {
 
       <Pane display='flex' width='100%' height='calc(100vh - 77px)'>
         <Pane display='flex' flexDirection='column' minWidth={450} overflowY='auto'>
-          <Heading size={600} margin={0} padding='1em' textAlign='center' backgroundColor='#f9f9fb'>
-            {title}
-          </Heading>
           {backButton && (
             <BackButton />
           )}
+          <Heading size={600} margin={0} padding='1em' textAlign='center' backgroundColor='#f9f9fb'>
+            {title}
+          </Heading>
           {children}
         </Pane>
 
