dc9f1482597b5e0c2f530aabf6762e54676d4d88
tmerlier <theophile.merliere@gmail.com>
2021-02-11 19:05:11 +0100

    Change style of back to bal list button

--- pages/new/back-to-user-bals.js
+++ pages/new/back-to-user-bals.js
@@ -7,7 +7,7 @@ const BackToUserBALs = () => {
   const access = getBalAccess()
   return Object.keys(access).length > 0 ? (
     <Pane marginLeft={16} marginTop={32}>
-      <BackButton appearance='primary' intent='primary' is='a' href='/'>Voir mes Bases Adresses Locales</BackButton>
+      <BackButton intent='primary' is='a' href='/'>Retour à la liste de mes Bases Adresses Locales</BackButton>
     </Pane>
   ) : null
 }
