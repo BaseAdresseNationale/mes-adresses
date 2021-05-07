3a0c22f7043da3e672ff3acb83e9ecf650b1e1f8
gllmr <guillaume@livingdata.co>
2021-05-07 11:15:33 +0200

    Remove unecessary href

--- components/header.js
+++ components/header.js
@@ -12,7 +12,7 @@ const Header = () => {
     <Pane borderBottom padding={16} backgroundColor='white' display='flex' justifyContent='space-between' alignItems='center' flexShrink='0' width='100%' maxHeight={76}>
       <Pane cursor='pointer'>
         <NextLink href='/'>
-          <a href='/'>
+          <a>
             <Image className='img' height='34' width='304' src='/static/images/mes-adresses.svg' alt='Page d’accueil du site mes-adresses.data.gouv.fr' />
           </a>
         </NextLink>
