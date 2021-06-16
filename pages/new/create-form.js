ef058509c2c28ccfe6f2ca7756091fcbe4046e22
gllmr <guillaume@livingdata.co>
2021-06-16 11:39:53 +0200

    Remove commune as array

--- pages/new/create-form.js
+++ pages/new/create-form.js
@@ -55,7 +55,7 @@ function CreateForm({defaultCommune}) {
     e.preventDefault()
     setIsLoading(true)
 
-    const isPublished = await isBalAlreadyPublished([commune], email)
+    const isPublished = await isBalAlreadyPublished(commune, email)
 
     if (isPublished.length > 0) {
       setAlreadyPublishedBAL(isPublished[0])
