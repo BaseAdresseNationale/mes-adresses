d09deb9b43f36615f2a9ae81ec663b45b06d00ce
gllmr <guillaume@livingdata.co>
2021-06-21 12:21:37 +0200

    Use an array & change variables name and wording

--- pages/new/alert-published-bal.js
+++ pages/new/alert-published-bal.js
@@ -7,7 +7,7 @@ import {getCommune} from '../../lib/geo-api'
 
 import BaseLocaleCard from '../../components/bases-locales-list/base-locale-card'
 
-const AlertPublishedBAL = ({isShown, onClose, onConfirm, alreadyPublishedBAL}) => {
+const AlertPublishedBAL = ({isShown, onClose, onConfirm, existingBALs}) => {
   const [commune, setCommune] = useState(null)
 
   useEffect(() => {
@@ -15,12 +15,8 @@ const AlertPublishedBAL = ({isShown, onClose, onConfirm, alreadyPublishedBAL}) =
       setCommune(await getCommune(code))
     }
 
-    if (alreadyPublishedBAL.length > 1) {
-      fetchCommune(alreadyPublishedBAL[0].communes[0])
-    } else {
-      fetchCommune(alreadyPublishedBAL.communes[0])
-    }
-  }, [alreadyPublishedBAL])
+    fetchCommune(existingBALs[0].communes[0])
+  }, [existingBALs])
 
   const onBalSelect = bal => {
     if (bal.communes.length === 1) {
@@ -37,10 +33,10 @@ const AlertPublishedBAL = ({isShown, onClose, onConfirm, alreadyPublishedBAL}) =
     <>
       <Dialog
         isShown={isShown}
-        title={alreadyPublishedBAL.isOwner ? (
-          'Vous avez déjà publié une Base Adresse Locale pour cette commune'
+        title={existingBALs.length > 1 ? (
+          'Vous avez déjà des Bases Adresses Locales existantes'
         ) : (
-          'Une Base Adresse Locale a déjà été publiée pour cette commune'
+          'Vous avez déjà publié une Base Adresse Locale pour cette commune'
         )}
         width='800px'
         intent='success'
@@ -51,25 +47,48 @@ const AlertPublishedBAL = ({isShown, onClose, onConfirm, alreadyPublishedBAL}) =
       >
         <Pane>
           <Alert margin='1em'>
-            <Text>
-              Une Base Adresse Locale de la commune de <b>{commune?.nom} </b>a déjà été publiée.
-            </Text>
-            <br />
-            <Text>
-              Vous pouvez reprendre votre travail sur cette Base Adresse Locale en cliquant sur &quot;Gérer mes adresses&quot; ou choisir d’en créer une nouvelle.
-            </Text>
-            <br />
-            <Text>
-              Nous vous <u>recommandons</u> toutefois de continuer l’adressage sur la Base Adresse Locale déjà publiée.
-            </Text>
+            {existingBALs.length > 1 ? (
+              <>
+                <Text>
+                  Des Bases Adresses Locales correpondantes existent dans notre base de données.
+                </Text>
+                <br />
+                <Text>
+                  Vous pouvez reprendre votre travail sur une de ces Bases Adresses Locales en cliquant sur &quot;Gérer mes adresses&quot; ou choisir d’en créer une nouvelle.
+                </Text>
+                <br />
+                <Text>
+                  Nous vous <u>recommandons</u> toutefois de continuer l’adressage sur une de vos Bases Adresses Locales déjà existantes.
+                </Text>
+              </>
+            ) : (
+              <>
+                <Text>
+                  Une Base Adresse Locale de la commune de <b>{commune?.nom} </b>a déjà été crée.
+                </Text>
+                <br />
+                <Text>
+                  Vous pouvez reprendre votre travail sur cette Base Adresse Locale en cliquant sur &quot;Gérer mes adresses&quot; ou choisir d’en créer une nouvelle.
+                </Text>
+                <br />
+                <Text>
+                  Nous vous <u>recommandons</u> toutefois de continuer l’adressage sur la Base Adresse Locale déjà existante.
+                </Text>
+              </>
+            )}
           </Alert>
-          {!alreadyPublishedBAL.length && (
-            <BaseLocaleCard
-              initialIsOpen
-              editable={alreadyPublishedBAL?.isOwner}
-              baseLocale={alreadyPublishedBAL}
-              onSelect={() => onBalSelect(alreadyPublishedBAL)}
-            />
+          {existingBALs.length > 0 && (
+            existingBALs.map(bal => {
+              return (
+                <BaseLocaleCard
+                  key={bal._id}
+                  initialIsOpen
+                  editable={existingBALs.length > 0}
+                  baseLocale={bal}
+                  onSelect={() => onBalSelect(bal)}
+                />
+              )
+            })
           )}
         </Pane>
       </Dialog>
@@ -80,7 +99,7 @@ const AlertPublishedBAL = ({isShown, onClose, onConfirm, alreadyPublishedBAL}) =
 AlertPublishedBAL.propTypes = {
   isShown: PropTypes.bool,
   onClose: PropTypes.func.isRequired,
-  alreadyPublishedBAL: PropTypes.object.isRequired,
+  existingBALs: PropTypes.object.isRequired,
   onConfirm: PropTypes.func.isRequired
 }
 
