cb2f18e7c798a079ddec65a9ba40f64bef50fdbc
tmerlier <theophile.merliere@gmail.com>
2021-07-28 12:05:49 +0200

    Create custom hook for localstorage

---
+++ hooks/local-storage.js
@@ -0,0 +1,48 @@
+import {useState, useEffect} from 'react'
+
+export function useLocalStorage(key, value) {
+  const [data, setData] = useState()
+
+  const getData = () => {
+    return JSON.parse(localStorage.getItem(key))
+  }
+
+  const storeData = value => {
+    localStorage.setItem(key, JSON.stringify(value))
+    setData(value)
+  }
+
+  const getIndex = index => {
+    const data = getData()
+
+    return data[index]
+  }
+
+  const addIndex = (index, value) => {
+    const previous = getData()
+
+    storeData({
+      ...previous,
+      [index]: value
+    })
+  }
+
+  const removeIndex = index => {
+    const data = getData()
+    delete data[index]
+
+    storeData(data)
+  }
+
+  // Initializes with data already stored if none is provided
+  useEffect(() => {
+    if (value) {
+      storeData(value)
+    } else {
+      const v = getData()
+      setData(v)
+    }
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  return [data, storeData, getIndex, addIndex, removeIndex]
+}
