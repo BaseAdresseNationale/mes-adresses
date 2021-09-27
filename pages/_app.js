e66d51d35c2d224fda3ea579cedad84ce9ec4f86
tmerlier <theophile.merliere@gmail.com>
2021-09-27 15:13:53 +0200

    Remove unused dependecies

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback, useMemo, useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 import Head from 'next/head'
 import {Pane, Dialog, Paragraph} from 'evergreen-ui'
