0803c3df006117cdfc2f1ef1a2e5fb6f0adec6af
Marine Le Pennec <lepennec.marine@gmail.com>
2021-09-30 16:11:48 +0200

    Improve component displayed conditions

--- pages/bal/voie.js
+++ pages/bal/voie.js
@@ -46,7 +46,7 @@ const Voie = React.memo(({voie, defaultNumeros}) => {
   const [selectedNumerosIds, setSelectedNumerosIds] = useState([])
   const [isAllSelectedCertifie, setIsAllSelectedCertifie] = useState(false)
 
-  const isGroupedActionsShown = useMemo(() => token && !isEditing && selectedNumerosIds.length > 1, [token, selectedNumerosIds, isEditing])
+  const isGroupedActionsShown = useMemo(() => token && selectedNumerosIds.length > 1, [token, selectedNumerosIds])
   const noFilter = numeros && filtered.length === numeros.length
   const allNumerosSelected = noFilter && (selectedNumerosIds.length === numeros.length)
   const allFilteredNumerosSelected = !noFilter && (filtered.length === selectedNumerosIds.length)
@@ -60,6 +60,8 @@ const Voie = React.memo(({voie, defaultNumeros}) => {
 
     return selectedNumerosIds.map(({_id}) => _id)
   }, [numeros, selectedNumerosIds, noFilter])
+
+  const editedNumero = filtered.find(numero => numero._id === editingId)
 
   const handleSelect = id => {
     setSelectedNumerosIds(selectedNumero => {
@@ -78,8 +80,6 @@ const Voie = React.memo(({voie, defaultNumeros}) => {
       setSelectedNumerosIds(filtered.map(({_id}) => _id))
     }
   }
-
-  const editedNumero = filtered.find(numero => numero._id === editingId)
 
   const onAdd = useCallback(async (voieData, body) => {
     let editedVoie = voieData
@@ -181,136 +181,144 @@ const Voie = React.memo(({voie, defaultNumeros}) => {
     <>
       <VoieHeading defaultVoie={voie} />
 
-      {!isEditing && (
-        <Pane
-          flexShrink={0}
-          elevation={0}
-          backgroundColor='white'
-          padding={16}
-          display='flex'
-          alignItems='center'
-          minHeight={64}
-        >
-          <Pane>
-            <Heading>Liste des numéros</Heading>
-          </Pane>
-          {token && (
-            <Pane marginLeft='auto'>
-              <Button
-                iconBefore={AddIcon}
-                appearance='primary'
-                intent='success'
-                onClick={onEnableAdding}
-              >
-                Ajouter un numéro
-              </Button>
-            </Pane>
+      {editedNumero ? (
+        <Pane flex={1} overflowY='scroll'>
+          <Table.Row height='auto'>
+            <Table.Cell display='block' paddingY={12} background='tint1'>
+              <NumeroEditor
+                initialVoieId={voie._id}
+                initialValue={editedNumero}
+                onSubmit={onEdit}
+                onCancel={onCancel}
+              />
+            </Table.Cell>
+          </Table.Row>
+
+          {error && (
+            <Alert marginY={5} intent='danger' title='Erreur'>
+              {error}
+            </Alert>
           )}
         </Pane>
-      )}
+      ) : (
+        <>
+          <Pane
+            flexShrink={0}
+            elevation={0}
+            backgroundColor='white'
+            padding={16}
+            display='flex'
+            alignItems='center'
+            minHeight={64}
+          >
+            <Pane>
+              <Heading>Liste des numéros</Heading>
+            </Pane>
 
-      {isGroupedActionsShown && numeros && (
-        <GroupedActions
-          idVoie={voie._id}
-          numeros={numeros}
-          selectedNumerosIds={toEdit}
-          resetSelectedNumerosIds={() => setSelectedNumerosIds([])}
-          setIsRemoveWarningShown={setIsRemoveWarningShown}
-          isAllSelectedCertifie={isAllSelectedCertifie}
-          onSubmit={onMultipleEdit}
-        />
-      )}
+            {token && (
+              <Pane marginLeft='auto'>
+                <Button
+                  iconBefore={AddIcon}
+                  appearance='primary'
+                  intent='success'
+                  disabled={isAdding || isEditing}
+                  onClick={onEnableAdding}
+                >
+                  Ajouter un numéro
+                </Button>
+              </Pane>
+            )}
+          </Pane>
 
-      <DeleteWarning
-        isShown={isRemoveWarningShown}
-        content={(
-          <Paragraph>
-            Êtes vous bien sûr de vouloir supprimer tous les numéros sélectionnés ?
-          </Paragraph>
-        )}
-        onCancel={() => setIsRemoveWarningShown(false)}
-        onConfirm={() => onMultipleRemove(toEdit)}
-      />
+          {isGroupedActionsShown && numeros && (
+            <GroupedActions
+              idVoie={voie._id}
+              numeros={numeros}
+              selectedNumerosIds={toEdit}
+              resetSelectedNumerosIds={() => setSelectedNumerosIds([])}
+              setIsRemoveWarningShown={setIsRemoveWarningShown}
+              isAllSelectedCertifie={isAllSelectedCertifie}
+              onSubmit={onMultipleEdit}
+            />
+          )}
 
-      {error && (
-        <Alert marginY={5} intent='danger' title='Erreur'>
-          {error}
-        </Alert>
-      )}
+          <DeleteWarning
+            isShown={isRemoveWarningShown}
+            content={(
+              <Paragraph>
+                Êtes vous bien sûr de vouloir supprimer tous les numéros sélectionnés ?
+              </Paragraph>
+            )}
+            onCancel={() => setIsRemoveWarningShown(false)}
+            onConfirm={() => onMultipleRemove(toEdit)}
+          />
 
-      <Pane flex={1} overflowY='scroll'>
-        <Table>
-          {!isEditing && (
-            <Table.Head>
-              {numeros && token && filtered.length > 1 && (
-                <Table.Cell flex='0 1 1'>
-                  <Checkbox
-                    checked={isAllSelected}
-                    onChange={handleSelectAll}
-                  />
-                </Table.Cell>
+          {error && (
+            <Alert marginY={5} intent='danger' title='Erreur'>
+              {error}
+            </Alert>
+          )}
+
+          <Pane flex={1} overflowY='scroll'>
+            <Table>
+              <Table.Head>
+                {numeros && token && filtered.length > 1 && (
+                  <Table.Cell flex='0 1 1'>
+                    <Checkbox
+                      checked={isAllSelected}
+                      onChange={handleSelectAll}
+                    />
+                  </Table.Cell>
+                )}
+                <Table.SearchHeaderCell
+                  placeholder='Rechercher un numéro'
+                  onChange={setFilter}
+                />
+              </Table.Head>
+
+              {isAdding && (
+                <Table.Row height='auto'>
+                  <Table.Cell borderBottom display='block' paddingY={12} background='tint1'>
+                    <NumeroEditor
+                      initialVoieId={voie._id}
+                      onSubmit={onAdd}
+                      onCancel={onCancel}
+                    />
+                  </Table.Cell>
+                </Table.Row>
               )}
-              <Table.SearchHeaderCell
-                placeholder='Rechercher un numéro'
-                onChange={setFilter}
+
+              {filtered.length === 0 && (
+                <Table.Row>
+                  <Table.TextCell color='muted' fontStyle='italic'>
+                    Aucun numéro
+                  </Table.TextCell>
+                </Table.Row>
+              )}
+            </Table>
+
+            {filtered.map(numero => (
+              <TableRow
+                {...numero}
+                key={numero._id}
+                id={numero._id}
+                isCertified={numero.certifie}
+                comment={numero.comment}
+                warning={numero.positions.find(p => p.type === 'inconnue') ? 'Le type d’une position est inconnu' : null}
+                isSelectable={!isEditing}
+                label={numero.numeroComplet}
+                secondary={numero.positions.length > 1 ? `${numero.positions.length} positions` : null}
+                toponymeId={numero.toponyme}
+                handleSelect={handleSelect}
+                isSelected={selectedNumerosIds.includes(numero._id)}
+                onEdit={onEnableEditing}
+                onRemove={onRemove}
               />
-            </Table.Head>
-          )}
-
-          {isAdding && (
-            <Table.Row height='auto'>
-              <Table.Cell borderBottom display='block' paddingY={12} background='tint1'>
-                <NumeroEditor
-                  initialVoieId={voie._id}
-                  onSubmit={onAdd}
-                  onCancel={onCancel}
-                />
-              </Table.Cell>
-            </Table.Row>
-          )}
-          {filtered.length === 0 && (
-            <Table.Row>
-              <Table.TextCell color='muted' fontStyle='italic'>
-                Aucun numéro
-              </Table.TextCell>
-            </Table.Row>
-          )}
-
-          {editingId && editingId !== voie._id ? (
-            <Table.Row height='auto'>
-              <Table.Cell display='block' paddingY={12} background='tint1'>
-                <NumeroEditor
-                  initialVoieId={voie._id}
-                  initialValue={editedNumero}
-                  onSubmit={onEdit}
-                  onCancel={onCancel}
-                />
-              </Table.Cell>
-            </Table.Row>
-          ) : (
-            !isEditing && (
-              filtered.map(numero => (
-                <TableRow
-                  {...numero}
-                  key={numero._id}
-                  id={numero._id}
-                  isCertified={numero.certifie}
-                  comment={numero.comment}
-                  warning={numero.positions.find(p => p.type === 'inconnue') ? 'Le type d’une position est inconnu' : null}
-                  isSelectable={!isEditing}
-                  label={numero.numeroComplet}
-                  secondary={numero.positions.length > 1 ? `${numero.positions.length} positions` : null}
-                  toponymeId={numero.toponyme}
-                  handleSelect={handleSelect}
-                  isSelected={selectedNumerosIds.includes(numero._id)}
-                  onEdit={onEnableEditing}
-                  onRemove={onRemove}
-                />
-              ))
-            )
-          )}
-        </Table>
-      </Pane>
+            ))}
+          </Pane>
+        </>
+      )
+      }
     </>
   )
 })
