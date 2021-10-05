9e7a8a23ea70b1ae2f4d441079c9ee9a24cd5c6f
tmerlier <theophile.merliere@gmail.com>
2021-10-05 14:57:36 +0200

    Add content to error page

--- pages/error.js
+++ pages/error.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Pane, Heading} from 'evergreen-ui'
+import {Pane, Heading, Button, Icon, ArrowLeftIcon, RouteIcon} from 'evergreen-ui'
 
 import Header from '../components/header'
 
@@ -13,10 +13,13 @@ function NotFound({statusCode}) {
         alignItems='center'
         flexDirection='column'
         justifyContent='center'
-        border='default'
-        height='100%'
+        height='50%'
       >
-        <Heading size={600} marginBottom='2em'>Erreur {statusCode} - {statusCode === 404 ? 'Page introuvable' : 'Une erreur est survenue'}</Heading>
+        <Icon icon={RouteIcon} size={100} marginX='auto' marginY={16} color='#101840' />
+        <Heading size={800} marginBottom='2em'>Erreur {statusCode} - {statusCode === 404 ? 'Page introuvable' : 'Une erreur est survenue'}</Heading>
+        <Button href='/' iconBefore={ArrowLeftIcon}>
+          Retour à la page d’accueil
+        </Button>
       </Pane>
     </Pane>
   )
