7c7f0ad9bd47a9d88fd713090cf064991f298407
Marine Le Pennec <lepennec.marine@gmail.com>
2021-10-28 16:38:59 +0200

    Add form component

---
+++ components/form.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Pane} from 'evergreen-ui'
+
+function Form({children, onFormSubmit}) {
+  return (
+    <Pane is='form' background='gray400' flex={1} padding={12} height='100%' onSubmit={onFormSubmit}>
+      {children}
+    </Pane>
+  )
+}
+
+Form.propTypes = {
+  children: PropTypes.node.isRequired,
+  onFormSubmit: PropTypes.func.isRequired
+}
+
+export default Form
