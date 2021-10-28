e61d6ac9b7961f18b58dfe693fe541ff2e52401d
Marine Le Pennec <lepennec.marine@gmail.com>
2021-10-28 16:39:12 +0200

    Add input wrapper

---
+++ components/form-input.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Pane} from 'evergreen-ui'
+
+function FormInput({children}) {
+  return (
+    <Pane background='white' padding={8} borderRadius={8} marginBottom={8} width='100%'>
+      {children}
+    </Pane>
+  )
+}
+
+FormInput.propTypes = {
+  children: PropTypes.node.isRequired
+}
+export default FormInput
