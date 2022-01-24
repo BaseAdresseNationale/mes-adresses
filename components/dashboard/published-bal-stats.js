f2073a407dbd0615579bc2a1328ada2a58cc1217
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2022-01-24 18:00:22 +0400

    Create PublishedBalStats component

---
+++ components/dashboard/published-bal-stats.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+import {Heading, Pane} from 'evergreen-ui'
+
+import Counter from './counter'
+
+function PublishedBalStats({stats}) {
+  const {nbVoies, nbLieuxDits, nbNumerosCertifies} = stats
+
+  return (
+    <Pane>
+      <Heading size={500} color='muted' fontWeight={300} textAlign='center'>Chiffres des Bases Adresses Locales publiées</Heading>
+      <Pane display='grid' gridTemplateColumns='repeat(3, 1fr)'>
+        <Counter label={nbVoies > 1 ? 'Voies' : 'Voie'} value={nbVoies} />
+        <Counter label={nbLieuxDits > 1 ? 'Lieux-dits' : 'Lieu-dit'} value={nbLieuxDits} />
+        <Counter label={nbNumerosCertifies > 1 ? 'Adresses certifiées' : 'Adresse certifiée'} value={nbNumerosCertifies} />
+      </Pane>
+    </Pane>
+
+  )
+}
+
+PublishedBalStats.propTypes = {
+  stats: PropTypes.object.isRequired
+}
+
+export default PublishedBalStats
