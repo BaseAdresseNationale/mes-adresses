4e49b5c73d5841c2f54993aaae5c0e16ad972b57
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2022-02-15 17:10:07 +0400

    Improve HiddenBal component

--- components/hidden-bal.js
+++ components/hidden-bal.js
@@ -1,34 +1,21 @@
-import {useContext, useState, useEffect} from 'react'
+import {useContext} from 'react'
 import {Alert, Button, Text} from 'evergreen-ui'
 
 import LocalStorageContext from '../contexts/local-storage'
 
 function HiddenBal() {
   const {setHiddenBal, hiddenBal} = useContext(LocalStorageContext)
-  const [hiddenBalList, setHiddenBalList] = useState([])
-
-  useEffect(() => {
-    if (hiddenBal) {
-      setHiddenBalList(Object.keys(hiddenBal))
-    } else {
-      setHiddenBalList([])
-    }
-  }, [hiddenBal])
-
-  const showAllBal = () => {
-    setHiddenBal(null)
-    setHiddenBalList([])
-  }
+  const isPlural = hiddenBal ? Object.keys(hiddenBal).length > 1 : false
 
   return (
     <div>
-      {hiddenBalList.length > 0 && (
+      {hiddenBal && Object.keys(hiddenBal).length > 0 && (
         <Alert intent='warning'>
           <Text>
-            Il y a {hiddenBalList.length} base{hiddenBalList.length > 1 ? 's' : ''} adresse{hiddenBalList.length > 1 ? 's' : ''} locale{hiddenBalList.length > 1 ? 's' : ''} non listée{hiddenBalList.length > 1 ? 's' : ''}.
-            Pour l{hiddenBalList.length > 1 ? 'es ' : '’'}afficher
+            Il y a {Object.keys(hiddenBal).length} base{isPlural ? 's' : ''} adresse{isPlural ? 's' : ''} locale{isPlural ? 's' : ''} non listée{isPlural ? 's' : ''}.
+            Pour l{isPlural ? 'es ' : '’'}afficher
           </Text>
-          <Button appearance='primary' marginLeft='1em' onClick={() => showAllBal()}>Cliquez ici</Button>
+          <Button appearance='primary' marginLeft='1em' onClick={() => setHiddenBal(null)}>Cliquez ici</Button>
         </Alert>
       )}
     </div>
