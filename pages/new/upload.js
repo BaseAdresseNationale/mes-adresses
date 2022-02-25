29c0fca6044ae044225f05dda3f7ea22034f0a54
Vincent Sagniez <vincent.sagniez@ign.fr>
2022-02-25 10:56:30 +0100

    Maj autres fichiers

--- pages/new/upload.js
+++ pages/new/upload.js
@@ -1,14 +1,8 @@
-import {useCallback} from 'react'
-import Router from 'next/router'
 import {Pane, Heading, Paragraph, Button} from 'evergreen-ui'
-
+import Link from 'next/link'
 import UploadForm from './upload-form'
 
 function Index() {
-  const onCreateNew = useCallback(() => {
-    Router.push('/new')
-  }, [])
-
   return (
     <>
       <Pane borderBottom padding={16} backgroundColor='white'>
@@ -24,9 +18,11 @@ function Index() {
         <Paragraph size={300} color='muted'>
           Vous pouvez créer une nouvelle Base Adresse Locale à partir de la commune de votre choix.
         </Paragraph>
-        <Button marginTop={10} onClick={onCreateNew}>
-          Créer une nouvelle Base Adresse Locale à partir d’une commune
-        </Button>
+        <Link href='/new' passHref>
+          <Button marginTop={10} is='a'>
+            Créer une nouvelle Base Adresse Locale à partir d’une commune
+          </Button>
+        </Link>
       </Pane>
     </>
   )
