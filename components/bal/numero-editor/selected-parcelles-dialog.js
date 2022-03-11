9250e176fa6f3b869f9c6377b9242a94c7d45796
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2022-03-11 17:29:03 +0400

    Create SelectedParcellesDialog component

---
+++ components/bal/numero-editor/selected-parcelles-dialog.js
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types'
+import {Badge, TrashIcon, Dialog} from 'evergreen-ui'
+
+function SelectedParcellesDialog({selectedParcelles, hoveredParcelle, handleParcelle, handleHoveredParcelle, isShown, setIsShown}) {
+  return (
+    <Dialog
+      hasFooter={false}
+      isShown={isShown}
+      title='Parcelles sélectionnées'
+      onCloseComplete={() => setIsShown(false)}
+    >
+      {selectedParcelles.slice(3).map(parcelle => {
+        const isHovered = parcelle === hoveredParcelle?.id
+
+        return (
+          <Badge
+            key={parcelle}
+            isInteractive
+            color={parcelle === hoveredParcelle?.id ? 'red' : 'green'}
+            margin={4}
+            onClick={() => handleParcelle(parcelle)}
+            onMouseEnter={() => handleHoveredParcelle({id: parcelle})}
+            onMouseLeave={() => handleHoveredParcelle(null)}
+          >
+            {parcelle}{isHovered && <TrashIcon marginLeft={4} size={14} color='danger' verticalAlign='text-bottom' />}
+          </Badge>
+        )
+      })}
+    </Dialog>
+  )
+}
+
+SelectedParcellesDialog.defaultProps = {
+  hoveredParcelle: null
+}
+
+SelectedParcellesDialog.propTypes = {
+  selectedParcelles: PropTypes.array.isRequired,
+  hoveredParcelle: PropTypes.object,
+  handleParcelle: PropTypes.func.isRequired,
+  handleHoveredParcelle: PropTypes.func.isRequired,
+  isShown: PropTypes.bool.isRequired,
+  setIsShown: PropTypes.func.isRequired
+}
+
+export default SelectedParcellesDialog
