132b43d4641a7cc53ad85ec475ea9579d3138648
tmerlier <theophile.merliere@gmail.com>
2022-03-16 12:00:17 +0100

    Use main layout for error page

--- pages/_error.js
+++ pages/_error.js
@@ -2,6 +2,8 @@ import PropTypes from 'prop-types'
 import Link from 'next/link'
 import {Pane, Heading, Button, Icon, ArrowLeftIcon, ErrorIcon, Alert, Text} from 'evergreen-ui'
 
+import Main from '@/layouts/main'
+
 import Custom404 from '@/pages/404'
 
 import Header from '@/components/header'
@@ -12,10 +14,11 @@ function CustomError({statusCode}) {
   }
 
   return (
-    <Pane display='flex' backgroundColor='#fff' flexDirection='column' width='100%' height='100%'>
+    <Main>
       <Header />
       <Pane
         display='flex'
+        flex={1}
         alignItems='center'
         flexDirection='column'
         justifyContent='center'
@@ -34,7 +37,7 @@ function CustomError({statusCode}) {
           </Button>
         </Link>
       </Pane>
-    </Pane>
+    </Main>
   )
 }
 
