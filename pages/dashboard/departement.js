749bb9a07ea26e021eb2290ab35d69f73e5ee844
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2022-04-05 20:56:54 +0400

    Improve dashboard/departement page

--- pages/dashboard/departement.js
+++ pages/dashboard/departement.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import {Heading, Pane} from 'evergreen-ui'
-import {flatten, groupBy, uniq} from 'lodash'
+import {flatten} from 'lodash'
 
 import {listBALByCodeDepartement} from '@/lib/bal-api'
 import {getDepartement, searchCommunesByCode} from '@/lib/geo-api'
@@ -13,24 +13,21 @@ import PieChart from '@/components/dashboard/charts/pie-chart'
 import CommuneBALList from '@/components/dashboard/commune-bal-list'
 import PublishedBalStats from '@/components/dashboard/published-bal-stats'
 
-function Departement({departement, filteredCommunesInBAL, basesLocalesDepartementWithoutDemo, BALGroupedByCommune, stats}) {
+function Departement({departement, filteredCommunesInBAL, basesLocalesDepartement, BALGroupedByCommune, stats, basesLocalesStatsByStatus, countCommunesActuelles}) {
   const {nom, code} = departement
   const {nbCommunes, nbVoies, nbLieuxDits, nbNumeros, nbNumerosCertifies} = stats
-  const codesCommunes = new Set(filteredCommunesInBAL.map(({code}) => code))
-  const communesWithoutTest = uniq(flatten(basesLocalesDepartementWithoutDemo.map(({communes}) => communes)))
-  const countCommunesActuelles = communesWithoutTest.filter(c => codesCommunes.has(c)).length
 
-  const BALByStatus = getBALByStatus(basesLocalesDepartementWithoutDemo)
+  const BALByStatus = getBALByStatus(basesLocalesStatsByStatus)
   const countPublishedBAL = BALByStatus.find(({label}) => label === 'Publiée' || label === 'Publiées')
 
   const mapData = {
     departement: code,
-    basesLocales: basesLocalesDepartementWithoutDemo
+    basesLocales: basesLocalesDepartement
   }
 
   return (
     <DashboardLayout backButton title={`Tableau de bord de l'éditeur Mes Adresses - ${nom} (${code})`} mapData={mapData}>
-      {basesLocalesDepartementWithoutDemo.length > 0 ? (
+      {basesLocalesDepartement.length > 0 ? (
         <Pane display='grid' gridGap='2em' padding={8}>
 
           {countPublishedBAL?.values > 0 && (
@@ -46,8 +43,8 @@ function Departement({departement, filteredCommunesInBAL, basesLocalesDepartemen
 
           <Pane display='flex' flexDirection='column' alignItems='center' >
             <Counter
-              label={`${basesLocalesDepartementWithoutDemo.length > 1 ? 'Bases Adresses Locales' : 'Base Adresse Locale'}`}
-              value={basesLocalesDepartementWithoutDemo.length} />
+              label={`${basesLocalesDepartement.length > 1 ? 'Bases Adresses Locales' : 'Base Adresse Locale'}`}
+              value={basesLocalesDepartement.length} />
             <PieChart height={240} data={BALByStatus} />
           </Pane>
 
@@ -76,33 +73,34 @@ Departement.getInitialProps = async ({query}) => {
   const {codeDepartement} = query
 
   const departement = await getDepartement(codeDepartement)
-  const basesLocalesDepartement = await listBALByCodeDepartement(codeDepartement)
-  const basesLocalesDepartementWithoutDemo = basesLocalesDepartement.basesLocales.filter(b => b.status !== 'demo')
+  const {basesLocales, BALGroupedByCommune, stats, codesCommunes} = await listBALByCodeDepartement(codeDepartement)
 
-  const BALAddedOneCodeCommune = flatten(basesLocalesDepartementWithoutDemo.map(b => b.communes.map(c => ({...b, commune: c}))))
-  const BALGroupedByCommune = groupBy(BALAddedOneCodeCommune, 'commune')
-
-  const communesActuelles = flatten(await Promise.all(Object.keys(BALGroupedByCommune).map(async c => {
+  const communesActuelles = flatten(await Promise.all(codesCommunes.map(async c => {
     const communes = await searchCommunesByCode(c)
     return communes
   })))
 
-  const filteredCommunesInBAL = communesActuelles.filter(({code}) => Object.keys(BALGroupedByCommune).includes(code))
+  const filteredCommunesInBAL = communesActuelles.filter(({code}) => codesCommunes.includes(code))
 
   return {
     departement,
     filteredCommunesInBAL,
-    basesLocalesDepartementWithoutDemo,
+    basesLocalesDepartement: basesLocales,
     BALGroupedByCommune,
-    stats: basesLocalesDepartement.stats,
+    stats: stats.basesLocalesStats,
+    basesLocalesStatsByStatus: stats.basesLocalesStatsByStatus,
+    countCommunesActuelles: communesActuelles.length,
+    layout: 'fullscreen'
   }
 }
 
 Departement.propTypes = {
   departement: PropTypes.object.isRequired,
   filteredCommunesInBAL: PropTypes.array.isRequired,
-  basesLocalesDepartementWithoutDemo: PropTypes.array.isRequired,
+  basesLocalesDepartement: PropTypes.array.isRequired,
   BALGroupedByCommune: PropTypes.object.isRequired,
+  basesLocalesStatsByStatus: PropTypes.object.isRequired,
+  countCommunesActuelles: PropTypes.number.isRequired,
   stats: PropTypes.object.isRequired
 }
 
