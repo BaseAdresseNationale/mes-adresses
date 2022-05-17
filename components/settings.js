c653be3d70dd6869ccd7aff8955430222d1d63eb
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2022-05-17 16:05:50 +0400

    Replace reloadCommune by reloadBaseLocale in Settings component

--- components/settings.js
+++ components/settings.js
@@ -9,13 +9,13 @@ import SettingsForm from '@/components/settings/settings-form'
 
 const Settings = React.memo(() => {
   const {isSettingsDisplayed, setIsSettingsDisplayed} = useContext(SettingsContext)
-  const {baseLocale, commune, reloadCommune} = useContext(BalDataContext)
+  const {baseLocale, reloadBaseLocale} = useContext(BalDataContext)
 
   useEffect(() => { // Update number of certified numeros when setting is open
     if (isSettingsDisplayed) {
-      reloadCommune()
+      reloadBaseLocale()
     }
-  }, [isSettingsDisplayed, reloadCommune])
+  }, [isSettingsDisplayed, reloadBaseLocale])
 
   return (
     <SideSheet
@@ -27,11 +27,9 @@ const Settings = React.memo(() => {
       <Pane borderBottom='1px solid #d8dae5' width='80%' marginY={16} marginX='auto' />
 
       <Pane padding={16}>
-        {commune && (
-          <Pane display='flex' justifyContent='center'>
-            <Certification nbNumeros={commune.nbNumeros} nbNumerosCertifies={commune.nbNumerosCertifies} onSubmit={() => reloadCommune()} />
-          </Pane>
-        )}
+        <Pane display='flex' justifyContent='center'>
+          <Certification nbNumeros={baseLocale.nbNumeros} nbNumerosCertifies={baseLocale.nbNumerosCertifies} onSubmit={() => reloadBaseLocale()} />
+        </Pane>
       </Pane>
 
       {baseLocale.status === 'demo' && (
