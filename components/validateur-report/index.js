5b684845bbddcbef006f30dc6f86ef6a27c95cac
Marine Le Pennec <lepennec.marine@gmail.com>
2022-08-16 14:46:24 +0200

    Delete find voie check

--- components/validateur-report/index.js
+++ components/validateur-report/index.js
@@ -26,7 +26,7 @@ function ValidateurReport({rows, voies, baseLocaleId}) {
       // For each voie, add voie's id, it's own alerts list and it's numeros with alerts list
       if (rowWithVoieAlerts.length > 0 || numerosWithAlerts.length > 0) {
         sanitizedRowsByVoies[nomVoie] = {
-          voieId: voies.find(voie => voie.nom === nomVoie)?._id,
+          voieId: voies.find(voie => voie.nom === nomVoie)._id,
           voieAlerts: rowWithVoieAlerts.length > 0 ? rowWithVoieAlerts[0].errors : [],
           numerosWithAlerts
         }
