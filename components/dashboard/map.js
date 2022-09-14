7159c4b28657a8a6deb52a2ce768963c1330269d
Cédric FAMIBELLE-PRONZOLA <c.pronzola@tuta.io>
2022-09-14 11:44:09 +0400

    Open bal list when clicking on map

--- components/dashboard/map.js
+++ components/dashboard/map.js
@@ -4,6 +4,8 @@ import {useRouter} from 'next/router'
 import MapGL, {Source, Layer, Popup, WebMercatorViewport} from 'react-map-gl'
 import {Paragraph, Heading, Text, Alert} from 'evergreen-ui'
 
+import BALListDialog from '@/components/dashboard/bal-list-dialog'
+
 import {colors} from '@/lib/colors'
 
 const BAL_API_URL = process.env.NEXT_PUBLIC_BAL_API_URL || 'https://api-bal.adresse.data.gouv.fr/v1'
@@ -26,6 +28,9 @@ function Map({departement, basesLocales}) {
   const [isTouchScreenDevice, setIsTouchScreenDevice] = useState(false)
   const [isDragPanEnabled, setIsDragPanEnabled] = useState(false)
   const [hoveredCommune, setHoveredCommune] = useState(null)
+  const [isShown, setIsShown] = useState(false)
+  const [selectedBasesLocales, setSelectedBasesLocales] = useState([])
+  const [hoveredCommuneWithBAL, setHoveredCommuneWithBAL] = useState(false)
 
   const router = useRouter()
 
@@ -86,6 +91,13 @@ function Map({departement, basesLocales}) {
         feature
       }
       const communeBALNumber = basesLocales.filter(({commune}) => commune === hoveredId).length
+      const balSourceLayer = event.features.find(({sourceLayer, properties}) => sourceLayer === 'communes' && properties.maxStatus !== 'published-other')
+
+      if (balSourceLayer) {
+        setHoveredCommuneWithBAL(true)
+      } else {
+        setHoveredCommuneWithBAL(false)
+      }
 
       setHovered(hoverInfo)
       setHoveredCommune(communeBALNumber)
@@ -140,9 +152,21 @@ function Map({departement, basesLocales}) {
     setIsZoomActivated(!isZoomActivated)
   }
 
+  const onCommuneSelect = basesLocales => {
+    setSelectedBasesLocales(basesLocales)
+    setIsShown(true)
+  }
+
   const handleClick = event => {
     event.stopPropagation()
     const departementsSourceLayer = event.features.find(({sourceLayer}) => sourceLayer === 'departements')
+    const balSourceLayer = event.features.find(({sourceLayer, properties}) => sourceLayer === 'communes' && properties.maxStatus !== 'published-other')
+
+    if (balSourceLayer) {
+      const filteredBasesLocales = basesLocales.filter(({commune}) => commune === balSourceLayer.properties.code)
+
+      onCommuneSelect(filteredBasesLocales)
+    }
 
     if (departementsSourceLayer) {
       const {code} = departementsSourceLayer.properties
@@ -214,6 +238,13 @@ function Map({departement, basesLocales}) {
 
   return (
     <div ref={mapContainerRef} className='map-container'>
+      <BALListDialog
+        basesLocales={selectedBasesLocales}
+        setSelectedBasesLocales={setSelectedBasesLocales}
+        isShown={isShown}
+        setIsShown={setIsShown}
+      />
+
       <MapGL
         {...viewport}
         ref={ref => {
@@ -230,7 +261,7 @@ function Map({departement, basesLocales}) {
         doubleClickZoom={false}
         scrollZoom={isZoomActivated}
         mapStyle='https://etalab-tiles.fr/styles/osm-bright/style.json'
-        getCursor={() => hoveredId ? 'pointer' : 'default'}
+        getCursor={() => hoveredId || hoveredCommuneWithBAL ? 'pointer' : 'default'}
         onDblClick={handleDoubleClick}
         onClick={handleClick}
         onViewportChange={setViewport}
