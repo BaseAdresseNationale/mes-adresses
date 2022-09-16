b1e7bd70ce5f5c23b6111031360007e3c1536379
Cédric FAMIBELLE-PRONZOLA <c.pronzola@tuta.io>
2022-09-16 09:18:43 +0400

    Create BALListDialog component

---
+++ components/dashboard/bal-list-dialog.js
@@ -0,0 +1,50 @@
+import {useCallback} from 'react'
+import Router from 'next/router'
+import PropTypes from 'prop-types'
+import {Pane, Dialog} from 'evergreen-ui'
+
+import BaseLocaleCard from '@/components/base-locale-card'
+
+function BALListDialog({basesLocales, setSelectedBasesLocales, isShown, setIsShown}) {
+  const handleClose = () => {
+    setIsShown(false)
+    setSelectedBasesLocales([])
+  }
+
+  const onBalSelect = useCallback(bal => {
+    Router.push(
+      `/bal?balId=${bal._id}`,
+      `/bal/${bal._id}`
+    )
+  }, [])
+
+  return (
+    <Pane>
+      <Dialog
+        isShown={isShown}
+        title={`${basesLocales.length > 1 ? 'Bases adresses locales' : 'Base adresse locale'}`}
+        onCloseComplete={handleClose}
+        hasFooter={false}
+      >
+        <Pane>
+          {basesLocales.map(bal => (
+            <BaseLocaleCard
+              key={bal._id}
+              baseLocale={bal}
+              onSelect={() => onBalSelect(bal)}
+            />
+          ))}
+        </Pane>
+      </Dialog>
+    </Pane>
+  )
+}
+
+BALListDialog.propTypes = {
+  basesLocales: PropTypes.array.isRequired,
+  setSelectedBasesLocales: PropTypes.func.isRequired,
+  isShown: PropTypes.bool.isRequired,
+  setIsShown: PropTypes.func.isRequired
+}
+
+export default BALListDialog
