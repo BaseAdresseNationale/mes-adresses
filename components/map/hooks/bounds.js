664127c7ff2933cac430395a5104b6145c29ecb0
Théophile Merlière <theophile.merliere@gmail.com>
2022-10-14 09:41:47 +0200

    Use geojson loading as trigger

--- components/map/hooks/bounds.js
+++ components/map/hooks/bounds.js
@@ -1,4 +1,4 @@
-import {useMemo, useContext, useState, useEffect, useRef} from 'react'
+import {useMemo, useContext, useState, useEffect} from 'react'
 import bbox from '@turf/bbox'
 
 import BalDataContext from '@/contexts/bal-data'
@@ -7,26 +7,26 @@ import {useRouter} from 'next/router'
 function useBounds(commune, toponyme) {
   const {geojson, editingItem} = useContext(BalDataContext)
   const [data, setData] = useState(commune.contour)
+  const [isGeojsonLoaded, setIsGeojsonLoaded] = useState(Boolean(geojson))
 
   const router = useRouter()
-  const geojsonFeatures = useRef(geojson?.features)
 
   useEffect(() => {
-    geojsonFeatures.current = geojson?.features
+    setIsGeojsonLoaded(Boolean(geojson))
   }, [geojson])
 
   useEffect(() => {
     let data
     const {idVoie, idToponyme} = router.query
 
-    if (geojsonFeatures.current) {
+    if (isGeojsonLoaded) {
       if (idVoie) {
         data = {
           type: 'FeatureCollection',
-          features: geojsonFeatures.current.filter(feature => feature.properties.idVoie === router.query.idVoie)
+          features: geojson.features.filter(feature => feature.properties.idVoie === router.query.idVoie)
         }
       } else if (idToponyme) {
-        const numeroToponyme = geojsonFeatures.current.filter(feature => feature.properties.idToponyme === idToponyme)
+        const numeroToponyme = geojson.features.filter(feature => feature.properties.idToponyme === idToponyme)
         data = {
           type: 'FeatureCollection',
           features: numeroToponyme.length === 0 && toponyme.positions.length === 1 ?
@@ -40,12 +40,13 @@ function useBounds(commune, toponyme) {
       }
     }
 
-    if (!geojsonFeatures.current || (!router.query.idVoie && !router.query.idToponyme)) {
+    if (!geojson || (!router.query.idVoie && !router.query.idToponyme)) {
       data = commune.contour
     }
 
     setData(data)
-  }, [commune.contour, router.query, toponyme])
+  }, [commune.contour, router.query, isGeojsonLoaded, toponyme]) // eslint-disable-line react-hooks/exhaustive-deps
+  // Use hasBound as hook instead of geojson to prevent fitBounds on numero update
 
   useEffect(() => {
     if (editingItem?.positions?.length > 1) {
