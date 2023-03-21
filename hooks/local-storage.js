c0f64a8b788a4165659cd8c6e4dac5b3201596b0
Guillaume Fay <guillaume_fay@hotmail.com>
2023-03-21 15:05:29 +0100

    fix: memoize useLocalStorage functions to avoid entering in a re-render loop

--- hooks/local-storage.js
+++ hooks/local-storage.js
@@ -1,44 +1,7 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useCallback} from 'react'
 
 export function useLocalStorage(key, value) {
   const [data, setData] = useState()
-
-  const getData = () => {
-    try {
-      return JSON.parse(localStorage.getItem(key))
-    } catch {
-      // Prevent from corrupt data like discovered with recovery-email-sent
-      storeData(null)
-      return null
-    }
-  }
-
-  const storeData = value => {
-    localStorage.setItem(key, JSON.stringify(value))
-    setData(value)
-  }
-
-  const getIndex = index => {
-    const data = getData()
-
-    return data ? data[index] : null
-  }
-
-  const addIndex = (index, value) => {
-    const previous = getData()
-
-    storeData({
-      ...previous,
-      [index]: value
-    })
-  }
-
-  const removeIndex = index => {
-    const data = getData()
-    delete data[index]
-
-    storeData(data)
-  }
 
   // Initializes with data already stored if none is provided
   useEffect(() => {
@@ -49,6 +12,43 @@ export function useLocalStorage(key, value) {
       setData(v)
     }
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  const storeData = useCallback(value => {
+    localStorage.setItem(key, JSON.stringify(value))
+    setData(value)
+  }, [key, setData])
+
+  const getData = useCallback(() => {
+    try {
+      return JSON.parse(localStorage.getItem(key))
+    } catch {
+      // Prevent from corrupt data like discovered with recovery-email-sent
+      storeData(null)
+      return null
+    }
+  }, [])
+
+  const getIndex = useCallback(index => {
+    const data = getData()
+
+    return data ? data[index] : null
+  }, [getData])
+
+  const addIndex = useCallback((index, value) => {
+    const previous = getData()
+
+    storeData({
+      ...previous,
+      [index]: value
+    })
+  }, [getData, storeData])
+
+  const removeIndex = useCallback(index => {
+    const data = getData()
+    delete data[index]
+
+    storeData(data)
+  }, [getData, storeData])
 
   return [data, storeData, getIndex, addIndex, removeIndex]
 }
