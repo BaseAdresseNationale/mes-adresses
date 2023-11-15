e9769a20927a627b71ace7b2c7533f568ef8e1b2
Fufeck <fabien.tafforeau@gmail.com>
2023-11-15 15:14:53 +0400

    correct trash

--- hooks/trash.js
+++ hooks/trash.js
@@ -1,8 +1,7 @@
 
 import {useCallback, useContext, useState} from 'react'
-import {groupBy} from 'lodash'
 
-import {getVoies, getNumerosByBal, getToponymes, restoreToponyme, removeToponyme, removeVoie, restoreVoie, removeMultipleNumeros} from '@/lib/bal-api'
+import {getAllDeleted, restoreToponyme, removeToponyme, removeVoie, restoreVoie, removeMultipleNumeros} from '@/lib/bal-api'
 
 import BalDataContext from '@/contexts/bal-data'
 import MapContext from '@/contexts/map'
@@ -22,62 +21,44 @@ function useTrash() {
   const [voiesDeleted, setVoiesDeleted] = useState([])
   const [toponymesDeleted, setToponymesDeleted] = useState([])
 
-  const reloadVoiesDeleted = useCallback(async () => {
-    const numerosDeleted = await getNumerosByBal(baseLocale._id, token, true)
-    const voies = await getVoies(baseLocale._id)
-    const numerosDeletedByVoie = groupBy(numerosDeleted, 'voie')
-
-    const result = []
-    for (const voieId in numerosDeletedByVoie) {
-      if (Object.hasOwnProperty.call(numerosDeletedByVoie, voieId)) {
-        const voieNumerosDeleted = numerosDeletedByVoie[voieId]
-        const voie = voies.find(({_id}) => _id === voieId)
-        voie.numeros = voieNumerosDeleted
-        result.push(voie)
-      }
-    }
-
-    setVoiesDeleted(result)
-  }, [baseLocale._id, token, setVoiesDeleted])
-
-  const reloadToponymesDelete = useCallback(async () => {
-    const res = await getToponymes(baseLocale._id, true)
-    setToponymesDeleted(res)
-  }, [baseLocale._id, setToponymesDeleted])
+  const reloadAllDeleted = useCallback(async () => {
+    const res = await getAllDeleted(baseLocale._id)
+    setToponymesDeleted(res.toponymes)
+    setVoiesDeleted(res.voies)
+  }, [baseLocale._id])
 
   const onRemoveVoie = useCallback(async voie => {
     const res = await removeVoie(voie._id, token)
     if (res) {
-      await reloadVoiesDeleted()
+      await reloadAllDeleted()
     }
-  }, [token, reloadVoiesDeleted])
+  }, [token, reloadAllDeleted])
 
   const onRestoreVoie = useCallback(async (voie, selectedNumerosIds) => {
     const res = await restoreVoie(voie._id, {numerosIds: selectedNumerosIds}, token)
-
     if (res) {
       await reloadVoies()
       await reloadNumeros()
       await reloadParcelles()
       reloadTiles()
-      await reloadVoiesDeleted()
+      await reloadAllDeleted()
       await refreshBALSync()
     }
-  }, [token, reloadNumeros, reloadParcelles, reloadVoies, reloadTiles, refreshBALSync, reloadVoiesDeleted])
+  }, [token, reloadNumeros, reloadParcelles, reloadVoies, reloadTiles, refreshBALSync, reloadAllDeleted])
 
   const onRemoveNumeros = useCallback(async voie => {
     const res = await removeMultipleNumeros(baseLocale._id, {numerosIds: voie.numeros.map(n => n._id)}, token)
     if (res) {
-      await reloadVoiesDeleted()
+      await reloadAllDeleted()
     }
-  }, [token, reloadVoiesDeleted, removeMultipleNumeros])
+  }, [baseLocale._id, token, reloadAllDeleted])
 
   const onRemoveToponyme = useCallback(async toponyme => {
     const res = await removeToponyme(toponyme._id, token)
     if (res) {
-      await reloadToponymesDelete()
+      await reloadAllDeleted()
     }
-  }, [token, reloadToponymesDelete])
+  }, [token, reloadAllDeleted])
 
   const onRestoreToponyme = useCallback(async toponyme => {
     const res = await restoreToponyme(toponyme._id, token)
@@ -87,9 +68,9 @@ function useTrash() {
       await reloadToponymes()
       reloadTiles()
       await refreshBALSync()
-      await reloadToponymesDelete()
+      await reloadAllDeleted()
     }
-  }, [token, reloadParcelles, reloadToponymes, reloadTiles, refreshBALSync, reloadToponymesDelete])
+  }, [token, reloadParcelles, reloadToponymes, reloadTiles, refreshBALSync, reloadAllDeleted])
 
   return {
     voiesDeleted,
@@ -99,8 +80,7 @@ function useTrash() {
     onRemoveNumeros,
     onRemoveToponyme,
     onRestoreToponyme,
-    reloadVoiesDeleted,
-    reloadToponymesDelete,
+    reloadAllDeleted,
   }
 }
 
