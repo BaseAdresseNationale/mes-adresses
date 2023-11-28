57812ff4a1274441e237c0c7d1605930f0fd3c92
Fufeck <fabien.tafforeau@gmail.com>
2023-11-28 11:22:04 +0400

    refacto pages/all

--- pages/all.tsx
+++ pages/all.tsx
@@ -1,14 +1,13 @@
-import React, {useState} from 'react'
+import React, {useState, } from 'react'
 import dynamic from 'next/dynamic'
 import Link from 'next/link'
-import {useRouter} from 'next/router'
+import {NextRouter, useRouter} from 'next/router'
 import {Pane, Heading, Paragraph, Button, Pagination, Spinner} from 'evergreen-ui'
 
-import {searchBasesLocales} from '@/lib/bal-api'
 import {sortBalByUpdate} from '@/lib/sort-bal'
 
 import Main from '@/layouts/main'
-import { BaseLocale } from '@/lib/openapi/index.js'
+import { BaseLocale, BasesLocalesService, PageBaseLocaleDTO } from '@/lib/openapi'
 
 interface PublicBasesLocalesListProps {
   basesLocales: BaseLocale[];
@@ -34,13 +33,13 @@ interface AllPageProps {
 }
 
 function All({basesLocales, commune, limit = 20, offset = 0, count = 0}: AllPageProps) {
-  const router = useRouter()
-  const totalPages = Math.ceil(count / limit)
-  const currentPage = Math.ceil((offset - 1) / limit) + 1
+  const router: NextRouter = useRouter()
+  const totalPages: number = Math.ceil(count / limit)
+  const currentPage: number = Math.ceil((offset - 1) / limit) + 1
 
-  const [input, setInput] = useState(commune || '')
+  const [input, setInput] = useState<string>(commune || '')
 
-  const onFilter = async value => {
+  const onFilter = async (value: string) => {
     const query = {page: currentPage, limit, commune: undefined}
 
     if (value.length > 0) {
@@ -50,14 +49,14 @@ function All({basesLocales, commune, limit = 20, offset = 0, count = 0}: AllPage
     await router.push({pathname: '/all', query})
   }
 
-  const handlePageChange = async page => {
+  const handlePageChange = async (page: number) => {
     await router.push({
       pathname: '/all',
       query: {page, limit}
     })
   }
 
-  const handleInput = value => {
+  const handleInput = (value: string) => {
     setInput(prev => {
       const input = value.slice(0, 5)
 
@@ -111,16 +110,24 @@ function All({basesLocales, commune, limit = 20, offset = 0, count = 0}: AllPage
 }
 
 All.getInitialProps = async ({query}) => {
-  let result
-
+  let result: PageBaseLocaleDTO
   try {
-    result = await searchBasesLocales(query)
-  } catch {}
+    result = await BasesLocalesService.searchBaseLocale(
+      query.limit,
+      query.offset,
+      query.deleted,
+      query.commune,
+      query.email,
+      query.status
+    )
+  } catch (e) {}
 
   return {
-    ...result,
+    basesLocales: result ? sortBalByUpdate(result.results) : [],
     commune: query.commune || '',
-    basesLocales: result ? sortBalByUpdate(result.results) : []
+    offset: result ? result.offset : 0,
+    limit: result ? result.limit : 20,
+    count: result ? result.count : 0,
   }
 }
 
