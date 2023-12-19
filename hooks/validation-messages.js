f5fbe93534e0bb57bc6f1f330d62a312b96e8813
Guillaume Fay <guillaume_fay@hotmail.com>
2023-12-19 15:18:42 +0100

    fix: validation messages

--- hooks/validation-messages.js
+++ hooks/validation-messages.js
@@ -10,11 +10,10 @@ export default function useValidationMessage() {
     }
   }, [])
 
-  const getValidationMessages = useCallback((field, index = null) => {
-    if (validationMessages && validationMessages[field]) {
-      return index === null ?
-        validationMessages[field] :
-        validationMessages[field][index]
+  const getValidationMessages = useCallback((field) => {
+    if (validationMessages && validationMessages.length > 0) {
+      const messages = validationMessages.filter(message => message.includes(field))
+      return messages && messages[0]
     }
   }, [validationMessages])
 
