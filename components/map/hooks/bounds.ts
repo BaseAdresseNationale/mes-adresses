709d4e6686c82d4a94a5653d232be0f0612b25ed
Guillaume Fay <guillaume_fay@hotmail.com>
2024-02-09 11:17:18 +0100

    feat: zoom on commune only once

---
+++ components/map/hooks/bounds.ts
@@ -0,0 +1,65 @@
+import { useMemo, useContext, useState, useEffect, useCallback } from "react";
+import bbox from "@turf/bbox";
+
+import BalDataContext from "@/contexts/bal-data";
+import MapContext from "@/contexts/map";
+import { useRouter } from "next/router";
+import { CommuneType } from "@/types/commune";
+import { Toponyme, Voie } from "@/lib/openapi";
+
+function useBounds(commune: CommuneType, voie: Voie, toponyme: Toponyme) {
+  const communeBounds = useMemo(
+    () => (commune.contour ? bbox(commune.contour) : null),
+    [commune.contour]
+  );
+  const { editingItem } = useContext(BalDataContext);
+  const { map, isTileSourceLoaded } = useContext(MapContext);
+  const [wasCenteredOnCommuneOnce, setWasCenteredOnCommuneOnce] =
+    useState(false);
+  const [bounds, setBounds] = useState(communeBounds);
+
+  const router = useRouter();
+
+  const setBoundsItem = useCallback(
+    (item) => {
+      if (map && item && item.trace) {
+        setBounds(bbox(item.trace));
+      } else if (map && item && item.bbox) {
+        setBounds(item.bbox);
+      }
+    },
+    [map, setBounds]
+  );
+
+  useEffect(() => {
+    // Get bounds on page load and when edit
+    const { idVoie, idToponyme } = router.query;
+    const bounds = communeBounds;
+
+    if (isTileSourceLoaded) {
+      if (idVoie) {
+        setBoundsItem(voie);
+      } else if (idToponyme) {
+        setBoundsItem(toponyme);
+      } else if (editingItem) {
+        setBoundsItem(editingItem);
+      } else if (!wasCenteredOnCommuneOnce) {
+        setBounds(bounds);
+        setWasCenteredOnCommuneOnce(true);
+      }
+    }
+  }, [
+    communeBounds,
+    router.query,
+    isTileSourceLoaded,
+    voie,
+    toponyme,
+    editingItem,
+    setBoundsItem,
+    wasCenteredOnCommuneOnce,
+  ]);
+
+  return bounds;
+}
+
+export default useBounds;
