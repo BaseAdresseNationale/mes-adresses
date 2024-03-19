76ed721197aed58a9ca0018ca1651b454efba91a
Guillaume Fay <guillaume_fay@hotmail.com>
2024-03-19 18:05:19 +0100

    feat: add signalement delete numero editor

---
+++ components/signalement/signalement-delete-numero-editor.tsx
@@ -0,0 +1,67 @@
+import { Button, Pane } from "evergreen-ui";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import NumeroEditor from "../bal/numero-editor";
+import { CommuneType } from "@/types/commune";
+import BalDataContext from "@/contexts/bal-data";
+import MapContext from "@/contexts/map";
+import { softRemoveNumero } from "@/lib/bal-api";
+import TokenContext from "@/contexts/token";
+
+interface SignalementDeleteNumeroEditorProps {
+  existingLocation: any;
+  handleSubmit: () => void;
+  handleClose: () => void;
+  commune: CommuneType;
+}
+
+function SignalementDeleteNumeroEditor({
+  existingLocation,
+  handleSubmit,
+  handleClose,
+  commune,
+}: SignalementDeleteNumeroEditorProps) {
+  const editorControlRef = useRef<HTMLDivElement>(null);
+  const { reloadNumeros, reloadParcelles, refreshBALSync } =
+    useContext(BalDataContext);
+  const { reloadTiles } = useContext(MapContext);
+  const { token } = useContext(TokenContext);
+
+  const onRemove = async (idNumero) => {
+    await softRemoveNumero(idNumero, token);
+    await reloadNumeros();
+    await reloadParcelles();
+    reloadTiles();
+    refreshBALSync();
+  };
+
+  const handleRemove = async (idNumero) => {
+    await onRemove(idNumero);
+    await handleSubmit();
+  };
+
+  return (
+    <Pane position="relative" height="100%">
+      <NumeroEditor
+        hasPreview
+        initialValue={existingLocation}
+        initialVoieId={existingLocation.voie?._id}
+        commune={commune}
+        closeForm={handleClose}
+        onSubmitted={handleSubmit}
+        certificationBtnChildren={
+          <Button
+            type="button"
+            appearance="primary"
+            intent="danger"
+            onClick={() => handleRemove(existingLocation._id)}
+          >
+            Supprimer
+          </Button>
+        }
+      />
+    </Pane>
+  );
+}
+
+export default SignalementDeleteNumeroEditor;
