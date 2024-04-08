97e561f128e79fab0bca4524793327ac1d4527a1
Fufeck <fabien.tafforeau@gmail.com>
2024-04-08 17:26:00 +0200

    feat: add alert anti spam

---
+++ components/habilitation-process/validate-authentication/index.tsx
@@ -0,0 +1,33 @@
+import { Pane, Button, ChevronLeftIcon } from "evergreen-ui";
+
+import CodeValidation from "@/components/habilitation-process/validate-authentication/code-validation";
+
+interface ValidateAuthenticationProps {
+  emailCommune: string;
+  validatePinCode: (code: string) => Promise<void>;
+  resendCode: () => Promise<void>;
+  onCancel: () => void;
+}
+
+function ValidateAuthentication({
+  emailCommune,
+  validatePinCode,
+  resendCode,
+  onCancel,
+}: ValidateAuthenticationProps) {
+  return (
+    <Pane>
+      <CodeValidation
+        email={emailCommune}
+        resendCode={resendCode}
+        handleSubmit={validatePinCode}
+      />
+
+      <Button iconBefore={ChevronLeftIcon} onClick={onCancel}>
+        Annuler
+      </Button>
+    </Pane>
+  );
+}
+
+export default ValidateAuthentication;
