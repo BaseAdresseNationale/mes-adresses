1055192e08b088721befba3676834625e236badf
Guillaume Fay <guillaume_fay@hotmail.com>
2024-07-19 16:50:57 +0200

    wip

--- e2e-tests/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test.beforeEach(async ({ page }) => {
-  await page.goto("/");
-});
-
-test("check title", async ({ page }) => {
-  await expect(page).toHaveTitle(/mes-adresses.data.gouv.fr/);
-});
