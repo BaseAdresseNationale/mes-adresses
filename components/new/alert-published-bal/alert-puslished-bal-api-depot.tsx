76dc07de8799d40619e297ac0a802e545b9e938f
Fufeck <fabien.tafforeau@gmail.com>
2024-08-20 12:21:48 +0200

    fix: popup create bal

--- components/new/alert-published-bal/alert-puslished-bal-api-depot.tsx
+++ components/new/alert-published-bal/alert-puslished-bal-api-depot.tsx
@@ -10,18 +10,25 @@ interface AlertAlertPublishedBALApiDepotProps {
 function AlertPublishedBALApiDepot({
   revision,
 }: AlertAlertPublishedBALApiDepotProps) {
-
   const client: Client = useMemo(() => {
-    return revision.client as Client
-  }, [revision])
+    return revision.client as Client;
+  }, [revision]);
 
   return (
     <Pane>
       <Paragraph marginTop={16}>
-        Une Base Adresse Locale est déjà gérée par <Strong>{client.chefDeFile}</Strong>.
-        { client.chefDeFileEmail &&
-          <>Nous vous recommandons de prendre contact avec <Strong>{client.chefDeFileEmail}</Strong> avant de poursuivre la création de votre BAL.</>
-        }
+        Une Base Adresse Locale est déjà gérée par{" "}
+        <Strong>
+          {client.chefDeFile ? client.chefDeFile : client.mandataire}
+        </Strong>
+        .
+        {client.chefDeFileEmail && (
+          <>
+            Nous vous recommandons de prendre contact avec{" "}
+            <Strong>{client.chefDeFileEmail}</Strong> avant de poursuivre la
+            création de votre BAL.
+          </>
+        )}
       </Paragraph>
       <Paragraph marginTop={16}>
         La commune reste toutefois l’autorité compétente en matière d’adressage,
