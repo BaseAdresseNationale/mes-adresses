e0e943e1786c421aa2e53eeb0a95a372f392505d
Fufeck <fabien.tafforeau@gmail.com>
2025-01-06 15:17:22 +0100

    fix: form display bad request langAlt and comment

--- components/comment.tsx
+++ components/comment.tsx
@@ -1,9 +1,10 @@
-import { Textarea, Label } from "evergreen-ui";
+import { Label, Text, TextareaField } from "evergreen-ui";
 
 import FormInput from "@/components/form-input";
 
 interface CommentProps {
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  validationMessage?: string;
   input?: string;
   limit?: number;
   isDisabled?: boolean;
@@ -11,6 +12,7 @@ interface CommentProps {
 
 function Comment({
   onChange,
+  validationMessage = null,
   input = "",
   limit = 5000,
   isDisabled = false,
@@ -20,11 +22,13 @@ function Comment({
       <Label marginBottom={4} display="block">
         Commentaire
       </Label>
-      <Textarea
+      <TextareaField
         placeholder="Cette note est confidentielle et ne sera pas diffusée."
         value={input}
         disabled={isDisabled}
         onChange={input.length < limit ? onChange : () => {}}
+        isInvalid={Boolean(validationMessage)}
+        validationMessage={validationMessage}
       />
     </FormInput>
   );
