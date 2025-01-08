bffd6b4ef665a7185b5990a4b0a1b691717ac55c
Fufeck <fabien.tafforeau@gmail.com>
2025-01-08 16:45:03 +0100

    feat: add nomAlt to commune

--- components/sidebar/header.tsx
+++ components/sidebar/header.tsx
@@ -1,20 +1,41 @@
-import React from "react";
-import { Heading, Pane, Text } from "evergreen-ui";
+import React, { useContext } from "react";
+import { EditIcon, Heading, Pane, Text } from "evergreen-ui";
 
-import { ExtendedVoieDTO } from "@/lib/openapi-api-bal";
+import { BaseLocale, ExtendedVoieDTO } from "@/lib/openapi-api-bal";
 import { CommuneType } from "@/types/commune";
+import LanguagePreview from "../bal/language-preview";
+import TokenContext from "@/contexts/token";
+import BalDataContext from "@/contexts/bal-data";
 
 interface HeaderSideBarProps {
+  baseLocale: BaseLocale;
   commune: CommuneType;
   voies: ExtendedVoieDTO[];
+  openForm: () => void;
 }
 
-function HeaderSideBar({ commune, voies }: HeaderSideBarProps) {
+function HeaderSideBar({
+  baseLocale,
+  commune,
+  voies,
+  openForm,
+}: HeaderSideBarProps) {
+  const { token } = useContext(TokenContext);
+  const { editingId, isEditing, numeros } = useContext(BalDataContext);
   return (
     <Pane display="flex" flexDirection="column" background="tint1" padding={16}>
       <Heading>
         {commune.nom} - {commune.code}
+        {!isEditing && token && (
+          <EditIcon
+            marginBottom={-2}
+            marginLeft={8}
+            color="black"
+            onClick={openForm}
+          />
+        )}
       </Heading>
+      {baseLocale.nomAlt && <LanguagePreview nomAlt={baseLocale.nomAlt} />}
       {voies && (
         <Text>
           {voies.length} voie{voies.length > 1 ? "s" : ""}
