79eb57ad88e1dbf84677c308147d7eefef737254
Fabien Tafforeau <fabien.tafforeau@gmail.com>
2025-12-16 14:52:39 +0100

    feat: accept numéro zero (#1072)

--- components/bal/numero-editor.tsx
+++ components/bal/numero-editor.tsx
@@ -399,8 +399,6 @@ function NumeroEditor({
               width="100%"
               maxWidth={300}
               flex={2}
-              min={0}
-              max={99999}
               value={numero}
               marginBottom={0}
               onWheel={(e) => e.target.blur()}
