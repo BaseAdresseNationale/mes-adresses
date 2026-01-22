7240b78bd931604f58ee2caa5de7bf5967c810b7
Guillaume Fay <guillaume_fay@hotmail.com>
2026-01-22 16:29:28 +0100

    fix: prettier config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,7 +1,9 @@
-export default PrettierConfig = {
+const prettierConfig = {
   semi: true,
   singleQuote: false,
   trailingComma: "all",
   printWidth: 100,
   tabWidth: 2,
 };
+
+export default prettierConfig;
