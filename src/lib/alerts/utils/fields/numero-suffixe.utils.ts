e50109742f3a8d5c1c90946845290ad4b3693ec9
Fufeck <fabien.tafforeau@gmail.com>
2026-02-16 17:01:49 +0100

    fix: bug no save numero

--- src/lib/alerts/utils/fields/numero-suffixe.utils.ts
+++ src/lib/alerts/utils/fields/numero-suffixe.utils.ts
@@ -3,6 +3,14 @@ import { AlertCodeNumeroEnum } from "../../alerts.types";
 export function computeNumeroSuffixeAlerts(
   value: string
 ): [codes: AlertCodeNumeroEnum[], remediation?: string] {
+  if (!/^[\da-z]/i.test(value)) {
+    return undefined;
+  }
+
+  if (value?.length > 9) {
+    return undefined;
+  }
+
   const alphanumericRegex =
     /^[a-zA-ZàáâãäåæçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝŸ0-9\s]+$/u;
   if (value && !value.match(alphanumericRegex)) {
