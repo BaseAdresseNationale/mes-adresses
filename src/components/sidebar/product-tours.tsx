ac71f67546c54a1ebccb4c99c1500566fa7b000b
Guillaume Fay <guillaume_fay@hotmail.com>
2026-04-08 16:03:40 +0200

    fix: signalement joyride triggered during habilitation process

--- src/components/sidebar/product-tours.tsx
+++ src/components/sidebar/product-tours.tsx
@@ -4,17 +4,19 @@ import SignalementContext from "@/contexts/signalement";
 import LocalStorageContext from "@/contexts/local-storage";
 import SignalementProductTour from "../signalement/product-tour/signalement-product-tour";
 import { CommuneType } from "@/types/commune";
+import BalDataContext from "@/contexts/bal-data";
 
 function ProductTours({ commune }: { commune: CommuneType }) {
   const { pendingSignalementsCount } = useContext(SignalementContext);
+  const { isHabilitationProcessDisplayed } = useContext(BalDataContext);
   const { wasWelcomed } = useContext(LocalStorageContext);
 
   return (
     <>
       <WelcomeMessage commune={commune} />
-      {pendingSignalementsCount > 0 && wasWelcomed && (
-        <SignalementProductTour />
-      )}
+      {pendingSignalementsCount > 0 &&
+        wasWelcomed &&
+        !isHabilitationProcessDisplayed && <SignalementProductTour />}
     </>
   );
 }
